6eb623aa08b56b78f4accb7e5d8dde2b483e11de
patrickbelding <52988299+patrickbelding@users.noreply.github.com>
2022-03-16 00:39:04 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -36,5 +36,6 @@ def create_app():
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
+        db.drop_all(app=app)
         db.create_all(app=app)
         print('Created Database!')
